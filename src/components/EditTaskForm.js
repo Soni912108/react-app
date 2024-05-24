6106303324deda46308e6a5a3ea98fec584227b2
erdison <109794675+Soni912108@users.noreply.github.com>
2024-05-24 13:55:35 +0200

    Update EditTaskForm.js

--- src/components/EditTaskForm.js
+++ src/components/EditTaskForm.js
@@ -18,7 +18,7 @@ const EditTaskForm = () => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch(`http://localhost:3001/api/tasks/${taskId}`);
+        const response = await fetch(`https://express-app-pied.vercel.app/api/tasks/${taskId}`);
         if (!response.ok) {
           throw new Error(`Error fetching task: ${response.statusText}`);
         }
@@ -46,7 +46,7 @@ const EditTaskForm = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:3001/api/tasks/${taskId}`, {
+      const response = await fetch(`https://express-app-pied.vercel.app/api/tasks/${taskId}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(task),
