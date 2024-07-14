8fad36bbf4de273ba02a6e1850aa2f4e6f523cb5
erdison <109794675+Soni912108@users.noreply.github.com>
2024-07-14 15:38:05 +0200

    Update TaskManager.js

--- src/components/TaskManager.js
+++ src/components/TaskManager.js
@@ -26,7 +26,7 @@ const TaskForm = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:3001/api/tasks/create', {
+      const response = await axios.post('https://express-app-pied.vercel.app/api/tasks/create', {
         name,
         description,
         completed,
@@ -55,7 +55,7 @@ const TaskForm = () => {
   const fetchTasks = useCallback(async () => {
     try {
       setLoading(true);
-      const response = await axios.get('http://localhost:3001/api/tasks/getAllTasks', {
+      const response = await axios.get('https://express-app-pied.vercel.app/api/tasks/getAllTasks', {
         headers: {
           'Authorization': `Bearer ${userToken}`,
         },
@@ -86,7 +86,7 @@ const TaskForm = () => {
 
   const deleteTask = async (taskId) => {
     try {
-      const response = await axios.delete(`http://localhost:3001/api/tasks/deleteTask/${taskId}`, {
+      const response = await axios.delete(`https://express-app-pied.vercel.app/api/tasks/deleteTask/${taskId}`, {
         headers: {
           'Authorization': `Bearer ${userToken}`,
         },
@@ -106,7 +106,7 @@ const TaskForm = () => {
 
   const handleTaskCompletion = async (taskID, isChecked) => {
     try {
-      const response = await axios.patch(`http://localhost:3001/api/tasks/updateTask/${taskID}`, {
+      const response = await axios.patch(`https://express-app-pied.vercel.app/api/tasks/updateTask/${taskID}`, {
         completed: isChecked,
       }, {
         headers: {
